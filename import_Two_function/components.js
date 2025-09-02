8e8ce111f067fd91424fa10d1cf979203ad80a4e
Vikas Meena <vikasbarlan1@gmail.com>
2025-09-02 21:55:24 +0530

    Create components.js

---
+++ import_Two_function/components.js
@@ -0,0 +1,15 @@
+import react from 'react';
+export function Meet(){
+    return (
+    <div>
+        <h2>Hi Vikas this if function Meet</h2>
+        </div>
+    );
+};
+export function Meeto(){
+    return (
+    <div>
+        <h2>This is from function Meeto</h2>
+        </div>
+    );
+};
