1e8cf9316a059df19d5055385c377bed704b6a09
Vikas Meena <vikasbarlan1@gmail.com>
2025-09-09 12:35:41 +0530

    Create index.js

---
+++ class/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import Car from "../classState.js";
+
+
+createRoot(document.getElementById("root")).render(<Car />);
