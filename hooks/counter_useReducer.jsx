52f712bb1961d4adb868612aa7a8a2be18a06e39
Vikas Meena <vikasbarlan1@gmail.com>
2025-11-08 12:38:32 +0530

    Update counter_useReducer.jsx

--- hooks/counter_useReducer.jsx
+++ hooks/counter_useReducer.jsx
@@ -1,27 +1,25 @@
 import { useReducer } from "react";
 import { createRoot } from "react-dom/client";
 
-function reducer(state,action){
-  switch (action.type){
-    case "increment":
-        return {count:state.count+1};
-    case "decrement":
-        return {count:state.cunt-1};
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return { count: state.count + 1 };
+    case "sub":
+      return { count: state.count - 1 };
     default:
-        return state;        
+      return state;
   }
 }
 
 function Counter() {
-  const [state, dispatch] = useReducer(reducer, {count:0});
-
-  
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <div>
-      <p>Count: {state.count}</p>
-      <button onClick={add}>Increament</button>
+      <p>count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "add" })}>+</button>
+      <button onClick={() => dispatch({ type: "sub" })}>-</button>
     </div>
   );
 }
-
 createRoot(document.getElementById("root")).render(<Counter />);
