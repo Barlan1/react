78616076e17e1c92a2745f5701d0ac46dbfe10db
Vikas Meena <vikasbarlan1@gmail.com>
2025-11-17 01:41:58 +0530

    Create UserCard_message component for user details

---
+++ hooks/basics/UserCard_message.js
@@ -0,0 +1,11 @@
+import React from "react";
+export function UserCard_message({user, age, city}){
+  return(
+    <div>
+     <h2>User Card</h2>
+     <strong>Name: {name}</strong>
+     <P> Age:{age}</p>
+      <strong> City: {city}</strong>
+    </div>
+  );
+}
